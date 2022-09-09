91419383231ffe96e6d8c63c5fa1b86f2fb3d192
Josh Hobson <joshuahobson1995@gmail.com>
2022-09-08 18:17:12 -0600

    wow

--- swampi/main.js
+++ swampi/main.js
@@ -2,15 +2,32 @@ console.log('my javascript is working')
 
 let residentButton = document.querySelector('#button1')
 
-
+let residentsNames = document.querySelector('.residents')
 
 const residentButtonFunction = (event) => {
 console.log('button clicked')
-axios.get('https://swapi.dev/api/').then((res) =>{
+axios.get('https://swapi.dev/api/planets/?search=alderaan' ).then((res) => {
+    
     console.log(res.data)
+
+// let residentsA = document.getElementsById
+// console.log(residentsA)
+
+   
+    
+
+    // let residents = res.data
+
+
+    // for(let i = 0; i < res.data.length; i++){
+    //     let header2 = document.createElement('h2')
+    //     header2.innerHTML = `<h2>${residents[i]} </h2>`
+
+    //     document.body.appendChild(paragraph)
+    //   }
+    //unfinished
 })
 }
 
 
 residentButton.addEventListener('click', residentButtonFunction)
-
